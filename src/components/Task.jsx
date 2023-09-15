6d0322fc7badda58fe2e7029a258fb2889c0cebb
LarissonRivero <Larissonrivero2@gmail.com>
2023-09-15 13:49:45 -0300

    Culminacion de la logica y estilos

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,42 +1,53 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const Task = () => {
-
-    const [nameTask, setNameTask] = useState(" ");
+    const [nameTask, setNameTask] = useState("");
     const [listTask, setListTask] = useState([]);
-    const [campo, setCampo] = useState('false');
+    const [campo, setCampo] = useState(false);
 
     const sendForm = (e) => {
-    e.preventDefault()
+        e.preventDefault();
 
-    if(!nameTask.trim()){
-        setCampo('true')
-        return
-    }
+        if (!nameTask.trim()) {
+            setCampo(true);
+            return;
+        } else {
+            setCampo(false);
+        }
 
-    setListTask([...listTask, nameTask])
-    setNameTask(" ")
-    }
+        setListTask([...listTask, nameTask]);
+        setNameTask(" ");
+    };
 
     const captureTask = (e) => {
-    setNameTask(e.target.value)
-    }
-  
+        setNameTask(e.target.value);
+    };
+
+    const deleteTask = (index) => {
+        const newList = listTask.filter((task, i) => i !== index);
+        setListTask(newList);
+    };
+
     return (
         <>
             <div className="formTask">
                 <form onSubmit={sendForm}>
-                    <input type="text" onChange={captureTask} value= {nameTask} placeholder="Add task" />
-                    <button>Add</button>
+                    <input type="text" onChange={captureTask} value={nameTask} placeholder="Add task" />
+                    <button type="submit">Add</button>
                 </form>
                 <ul>
-                    {listTask.map(task => <li key = {task}>{task}</li>)}
+                    {listTask.map((task, index) => (
+                        <li key={index}>
+                            - {task}
+                            <button className='bDelete' onClick={() => deleteTask(index)}>X</button>
+                        </li>
+                    ))}
                 </ul>
-            {listTask.length === 0 ? <p>No hay Tareas Pendientes</p> : <p>Numero de Tareas: {listTask.length}</p>}
+                {listTask.length === 0 ? <h3>No hay Tareas Pendientes</h3> : <h3>Numero de Tareas: {listTask.length}</h3>}
             </div>
-            {campo === 'true' ? <p>El campo esta vacio</p> : <p>El campo no esta vacio</p>}
+            {campo ? <p className='alert'>El Campo Agregar Tarea no puede estar vacio</p> : null}
         </>
-    )
-}
+    );
+};
 
 export default Task;
